92d1637d4089118023a5d3c1704374889a5c76c3
yanniks <kontakt@yanniks.de>
2014-03-16 13:40:27 +0100

    make the app unprivileged
    Change-Id: I1279f6edd14992590bb4f6062d43437e3410e86e

--- Android.mk
+++ Android.mk
@@ -1,7 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_PRIVILEGED_MODULE := true
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
 LOCAL_PACKAGE_NAME := WWPF
 LOCAL_CERTIFICATE := platform
